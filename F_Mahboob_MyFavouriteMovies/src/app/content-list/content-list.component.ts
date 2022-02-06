6a8de1b1964a9fac2c795efb96cdbcd07ba59649
Fmahboob <W0766260@myscc.ca>
2022-02-06 09:49:14 -0500

    Add styles and colors

--- F_Mahboob_MyFavouriteMovies/src/app/content-list/content-list.component.ts
+++ F_Mahboob_MyFavouriteMovies/src/app/content-list/content-list.component.ts
@@ -1,4 +1,6 @@
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 import { Component, OnInit } from '@angular/core';
+import { title } from 'process';
 
 @Component({
   selector: 'app-content-list',
@@ -6,10 +8,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent implements OnInit {
+  list: Content[] = [];
 
-  constructor() { }
+  constructor() { 
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+
+    this.list = [{
+      id: '',
+      title: '',
+      description: '',
+      creator: '',
+      imgURL: '',
+      type: '',
+      tags: []
+    },{
+       id: 1,
+      title: '',
+      description: '',
+      creator: '',
+      imgURL: '',
+      type: '',
+      tags: []
+    },{
+    
+      id: 2,
+      title: '',
+      description: '',
+      creator: '',
+      imgURL: '',
+      type: '',
+      tags: []
+    },{
+    
+      id: 3,
+      title: '',
+      description: '',
+      creator: '',
+      imgURL: '',
+      type: '',
+      tags: []
+    },
+   {
+      id: 4,
+      title: '',
+      description: '',
+      creator: '',
+      imgURL: '',
+      type: '',
+      tags: []
+    },{
+  
+      id: 5,
+      title: '',
+      description: '',
+      creator: '',
+      imgURL: '',
+      type: '',
+      tags: []
+    }
+    ];
+  }
 }
+
