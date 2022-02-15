d704e7121a612868fab307e39e0cf968c1341833
Fmahboob <W0766260@myscc.ca>
2022-02-15 09:26:00 -0500

    fixed all errors

--- F_Mahboob_MyFavouriteMovies/src/app/content-list/content-list.component.ts
+++ F_Mahboob_MyFavouriteMovies/src/app/content-list/content-list.component.ts
@@ -1,6 +1,7 @@
-import { Content } from '@angular/compiler/src/render3/r3_ast';
+
 import { Component, OnInit } from '@angular/core';
 import { title } from 'process';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-list',
@@ -22,16 +23,14 @@ export class ContentListComponent implements OnInit {
       description: 'New York hairdresser Izzy seizes the chance to work at the wedding of a charming prince. When sparks start to fly between the two of them, love and duty are put to the test as the time of the wedding draws closer.',
       creator: 'Rick Jacobson',
       imgURL: 'https://m.media-amazon.com/images/M/MV5BNjc5ZTg0MjgtOGFiNy00YjE5LThlZmYtMzE3NjViZWI2OTU2XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg',
-      type: 'romantic comedy',
-      tags: []
+      type: 'romantic comedy'
     },{
        id: 1,
       title: 'it',
       description: 'Seven young outcasts in Derry, Maine, are about to face their worst nightmare -- an ancient, shape-shifting evil that emerges from the sewer every 27 years to prey on the town children. Banding together over the course of one horrifying summer, the friends must overcome their own personal fears to battle the murderous, bloodthirsty clown known as Pennywise.',
       creator: 'Andy Muschietti',
       imgURL: 'https://m.media-amazon.com/images/I/61LokfYMGnL._AC_SY679_.jpg',
-      type: 'horror',
-      tags: []
+      type: 'horror'
     },{
     
        id: 2,
@@ -39,8 +38,7 @@ export class ContentListComponent implements OnInit {
       description: 'When bratty 8-year-old Kevin McCallister (Macaulay Culkin) acts out the night before a family trip to Paris, his mother makes him sleep in the attic. After the McCallisters mistakenly leave for the airport without Kevin, he awakens to an empty house and assumes his wish to have no family has come true. But his excitement sours when he realizes that two con men (Joe Pesci, Daniel Stern) plan to rob the McCallister residence, and that he alone must protect the family home.',
       creator: 'Chris Columbus',
       imgURL: 'https://m.media-amazon.com/images/I/A1T+lZ6iUZL._AC_SL1500_.jpg',
-      type: 'comedy',
-      tags: []
+      type: 'comedy'
     },{
     
       id: 3,
@@ -48,8 +46,7 @@ export class ContentListComponent implements OnInit {
       description: 'With Spider-Man identity now revealed, our friendly neighborhood web-slinger is unmasked and no longer able to separate his normal life as Peter Parker from the high stakes of being a superhero. When Peter asks for help from Doctor Strange, the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.',
       creator: 'Jon Watts',
       imgURL: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x_masonry/spider-mannowayhome_lob_crd_03.jpg',
-      type: 'action',
-      tags: []
+      type: 'action'
     },
    {
       id: 4,
@@ -57,8 +54,8 @@ export class ContentListComponent implements OnInit {
       description: 'If they hear you, they hunt you. A family must live in silence to avoid mysterious creatures that hunt by sound. Knowing that even the slightest whisper or footstep can bring death, Evelyn and Lee are determined to find a way to protect their children while desperately searching for a way to fight back.',
       creator: 'John Krasinski',
       imgURL: 'https://m.media-amazon.com/images/I/A1zV1fxtMEL._AC_SL1500_.jpg',
-      type: 'Horror',
-      tags: []
+      type: 'Horror'
+      
     },{
   
       id: 5,
@@ -66,8 +63,8 @@ export class ContentListComponent implements OnInit {
       description: 'Thomas  a teenager, arrives in a glade at the center of a giant labyrinth. Like the other youths dumped there before him, he has no memory of his previous life. Thomas quickly becomes part of the group and soon after demonstrates a unique perspective that scores him a promotion to Runner status -- those who patrol the always-changing maze to find an escape route. Together with Teresa (Kaya Scodelario), the only female, Thomas tries to convince his cohorts that he knows a way out.',
       creator: 'Wes Ball',
       imgURL: 'https://lumiere-a.akamaihd.net/v1/images/image_c09a58e5.jpeg',
-      type: 'Action',
-      tags: []
+      type: 'Action'
+     
     }
     ];
   }
